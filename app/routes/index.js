1c6443962d0004664ab088e25fe52956a9861ec4
C0D1NGD0J0 <web-dev@United4Life.local>
2017-10-14 19:41:03 -0400

    refactored server startup code and file structure

---
+++ app/routes/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* OTHER ROUTES */
+router.use('/users', require('./users'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict";
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const port = (process.env.PORT || '3000');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// VIEW ENGINE
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// MIDDLEWARE
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//ROUTES
+app.use('/', require("../app/routes"));
 
-// catch 404 and forward to error handler
+// ERROR HANDLER
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// ERROR HANDLER
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -43,4 +42,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(port, (err) => {
+	if(err) return console.log(err);
+	console.log(`Server is live on port ${port}`);
+});
