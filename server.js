05907067524360be933357006aba978362b615ac
C0D1NGD0J0 <web-dev@United4Life.local>
2017-10-14 19:49:23 -0400

    fixed incorrect file path for routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTES
-app.use('/', require("../app/routes"));
+app.use('/', require("./app/routes"));
 
 // ERROR HANDLER
 app.use(function(req, res, next) {
